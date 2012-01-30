e160dfd922055fc143223b9f1051d9e715151a80
Randy Hoyt <randy@randyhoyt.com>
2012-01-30 13:25:46 -0600

    Incrementing version number

--- openphoto-wordpress.php
+++ openphoto-wordpress.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: OpenPhoto for WordPress
-Version: 0.9.4.1
+Version: 0.9.4.2
 Plugin URI: https://github.com/openphoto/openphoto-wordpress
 Author: Randy Hoyt, Randy Jensen
 Author URI: http://cultivatr.com/
